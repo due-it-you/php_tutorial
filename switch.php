f525dd06b02b99332485ebf02a801400ecd756e2
dueityou <dueityou@gmail.com>
2025-11-20 19:46:16 +0900

    elseifの書き方

--- switch.php
+++ switch.php
@@ -6,4 +6,13 @@
   } else {
     echo "sampleは5以下です";
   }
+
+// 複数の条件分岐
+  if ($sample == 10) {
+    echo "sampleは10です";
+  } elseif ($sample >= 5) {
+    echo "sampleは5以上です。";
+  } elseif ($sample <= 5) {
+    echo "sampleは5以下です。";
+  }
 ?>
