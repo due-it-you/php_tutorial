70b85bc4fc779fc1c08b29f9cc267ea089922457
dueityou <dueityou@gmail.com>
2025-11-23 17:16:39 +0900

    インデックスを指定して配列に代入

--- array.php
+++ array.php
@@ -1,7 +1,16 @@
 <?php
+  // 0から順に配列に代入
   $fruit[] = "リンゴ";
   $fruit[] = "ミカン";
   $fruit[] = "ブドウ";
+  echo $fruit[0];
+  echo $fruit[1];
+  echo $fruit[2];
+
+  // 番号を指定して配列に代入
+  $fruit[2] = "リンゴ";
+  $fruit[1] = "ミカン";
+  $fruit[0] = "ブドウ";
   echo $fruit[0];
   echo $fruit[1];
   echo $fruit[2];
