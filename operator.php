021a2fbf2f62d6f3e49edc8fe2f571fe06f4014f
dueityou <dueityou@gmail.com>
2025-12-01 16:05:18 +0900

    nullsafe演算子

--- operator.php
+++ operator.php
@@ -4,4 +4,16 @@
   # 左辺がnullなら、右辺を返す
   $word = $null_or_not ?? "単語"; 
   echo $word;
+
+  # nullsafe演算子
+  # アクセスする時に元のオブジェクトがnullの場合、そのままnullを返す。
+  # nullではないならそのままアクセスできる。
+  # 従来のようなnullチェックの処理を前に挟む必要が減る。
+  class person
+  {
+    public string $name = "初期の名前";
+  }
+  $person = new person;
+  # ?-> nullsafe演算子
+  echo $person?->name;
 ?>
