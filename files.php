17a0209e78725b6ddbdd5075aa678c45340225de
dueityou <dueityou@gmail.com>
2025-12-09 20:03:38 +0900

    ファイルの書き込み

--- files.php
+++ files.php
@@ -1,4 +1,5 @@
 <?php
+  // ファイルの読み取り
   $file = fopen("example.txt", "r");
   if ($file) {
     // 一行分の文字列を取得
@@ -11,4 +12,11 @@
   } else {
     echo 'Error opening file';
   }
+
+  // ファイルの書き込み
+  // a : 追記モード
+  $written_file = fopen('example.txt', "a");
+  fwrite($written_file, "New Data\n");
+  fclose($written_file);
+  echo file_get_contents("example.txt");
 ?>
